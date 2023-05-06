4320c74385a485617860155ae66b60dff2f20196
thepjos <josh.thephasdin@gmail.com>
2023-05-05 20:17:47 -0500

    added html css js from chat gpt

---
+++ script.js
@@ -0,0 +1,67 @@
+const squares = document.querySelectorAll('.square');
+const message = document.getElementById('message');
+const restartButton = document.getElementById('restart');
+
+let currentPlayer = 'X';
+let board = ['', '', '', '', '', '', '', '', ''];
+let gameOver = false;
+
+function handleMove(square, index) {
+	if (board[index] === '' && !gameOver) {
+		board[index] = currentPlayer;
+		square.classList.add(currentPlayer.toLowerCase());
+		checkForWinner();
+		currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+		message.textContent = `Player ${currentPlayer}'s turn`;
+	}
+}
+
+function checkForWinner() {
+	const winningCombos = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	];
+
+	for (let combo of winningCombos) {
+		const [a, b, c] = combo;
+		if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+			gameOver = true;
+			message.textContent = `Player ${currentPlayer} wins!`;
+			restartButton.style.display = 'block';
+			return;
+		}
+	}
+
+	if (board.every(square => square !== '')) {
+		gameOver = true;
+		message.textContent = "It's a tie!";
+		restartButton.style.display = 'block';
+		return;
+	}
+}
+
+function restart() {
+	currentPlayer = 'X';
+	board = ['', '', '', '', '', '', '', '', ''];
+	gameOver = false;
+	message.textContent = `Player ${currentPlayer}'s turn`;
+	restartButton.style.display = 'none';
+
+	for (let square of squares) {
+		square.classList.remove('x', 'o');
+	}
+
+	squares.forEach((square, index) => {
+		square.addEventListener('click', () => handleMove(square, index));
+	});
+}
+
+restart();
+
+restartButton.addEventListener('click', restart);

4f1bad06e0eca2da91c2d5fd0f6943881ce7d5a4
thepjos <josh.thephasdin@gmail.com>
2023-05-05 20:36:52 -0500

    added revised code from gpt

--- script.js
+++ script.js
@@ -7,44 +7,52 @@ let board = ['', '', '', '', '', '', '', '', ''];
 let gameOver = false;
 
 function handleMove(square, index) {
-	if (board[index] === '' && !gameOver) {
-		board[index] = currentPlayer;
-		square.classList.add(currentPlayer.toLowerCase());
-		checkForWinner();
-		currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-		message.textContent = `Player ${currentPlayer}'s turn`;
-	}
-}
+    if (board[index] === '' && !gameOver) {
+      board[index] = currentPlayer;
+      square.classList.add(currentPlayer.toLowerCase());
+      checkForWinner();
+      if (!gameOver) {
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        message.textContent = `Player ${currentPlayer}'s turn`;
+      }
+    }
+  }
+  
 
 function checkForWinner() {
-	const winningCombos = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6]
-	];
-
-	for (let combo of winningCombos) {
-		const [a, b, c] = combo;
-		if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-			gameOver = true;
-			message.textContent = `Player ${currentPlayer} wins!`;
-			restartButton.style.display = 'block';
-			return;
-		}
-	}
-
-	if (board.every(square => square !== '')) {
-		gameOver = true;
-		message.textContent = "It's a tie!";
-		restartButton.style.display = 'block';
-		return;
-	}
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+  
+    for (let combo of winningCombos) {
+      const [a, b, c] = combo;
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        gameOver = true;
+        showGameOverMessage(`Player ${currentPlayer} wins!`);
+        return;
+      }
+    }
+  
+    if (board.every(square => square !== '')) {
+      gameOver = true;
+      showGameOverMessage("It's a tie!");
+      return;
+    }
+  }
+  
+function showGameOverMessage(msg) {
+    message.textContent = msg;
+    restartButton.style.display = 'block';
 }
+  
+  
 
 function restart() {
 	currentPlayer = 'X';
